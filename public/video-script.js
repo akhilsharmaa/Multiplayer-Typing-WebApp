09927722714652d4185db49ec43bc80b3b4a7eb8
“Akhilesh <akhilsharma3332@email.com>
2024-09-10 21:12:09 +0530

    Video Feed Availabe.

---
+++ public/video-script.js
@@ -0,0 +1,51 @@
+// Function to make the video box draggable
+dragElement(document.getElementById("draggable-video"));
+let localVideoEle = document.getElementById("my-video-element");
+
+function dragElement(element) {
+
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  element.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // Get the mouse cursor position at startup
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // Calculate the new cursor position
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // Set the element's new position
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // Stop moving when mouse button is released
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+initializeLocalFeed();
+
+async function initializeLocalFeed()  {
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video:true
+    })
+
+    localVideoEle.srcObject = stream; 
+
+}
